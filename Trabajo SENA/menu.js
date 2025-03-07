184f967d15e0fd25a2136cbaae089ac6b8078709
Santiago Perdomo <santiagoperdomoangulo67@gmail.com>
2025-03-07 14:27:06 -0500

    Subiendo plantilla web

--- Trabajo SENA/menu.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var menu = document.getElementById("menuContent");
-    if (menu.style.display === "none" || menu.style.display === "") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
-}
